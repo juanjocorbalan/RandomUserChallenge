91761875f1cd10743500c62e893f527cf1043760
Juanjo Corbalan <juanjocorbalan@gmail.com>
2020-07-06 11:48:03 +0200

    Initial Commit

---
+++ RandomUserChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomUserChallenge
+//
+//  Created by Juanjo Corbalán on 06/07/2020.
+//  Copyright © 2020 Juanjo Corbalán. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
